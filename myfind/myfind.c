b8937f75640a68e83f0be1fd03eda2c8803d8cfb
Romeo Beck <romeo@linuxvm.linuxvm.at>
2015-03-11 15:09:09 +0100

    bugfixing myfind

--- myfind/myfind.c
+++ myfind/myfind.c
@@ -320,16 +320,12 @@ int check_user(struct stat fd_in, const char * const * parms, int parm_pos)
 	char *endptr = NULL;
 	int parmsint = 0;
 
-	/* TODO: Error checking */
 	userdet = getpwuid(fd_in.st_uid);
 	parmsint = strtol(parms[parm_pos + 1], &endptr, 10);
 
-	if((strcmp(userdet->pw_name, parms[parm_pos + 1]) == 0) || (userdet->pw_uid == (uid_t)parmsint)) {
-		return MATCH;
-	} else {
-		return MISMATCH;
-	}
-
+	if((endptr != 0) & (strcmp(userdet->pw_name, parms[parm_pos + 1]) == 0)) return MATCH;
+	else if((endptr != 0) & (userdet->pw_uid == (uid_t)parmsint)) return MATCH;
+	else return MISMATCH;
 
 }
 
