9d5d4a7bdb43f28be001f5585e854d8c69ba76a0
Tom ic14b033 <ic14b033@technikum-wien.at>
2015-03-13 17:42:36 +0100

    error handling in check_user

--- myfind/myfind.c
+++ myfind/myfind.c
@@ -449,12 +449,15 @@ int check_user(struct stat fd_in, const char * const * parms, int parm_pos)
 	usernam = getpwnam(parms[parm_pos +1]);
 	/* if enterd user is a user name: check if the user exists */
 	if (usernam == NULL) {
-		if (errno == 0) error(0, 0, "%s is not the name of a known user", parms[parm_pos +1]);
+		error(1, 1, "%d", errno);
 		return MISMATCH;
 	}
 	/* get user data with UID */
 	userdet = getpwuid(fd_in.st_uid);
-	if (userdet == NULL) return MISMATCH;
+	if (userdet == NULL) {
+		error(1, 1, "%d", errno);
+		return MISMATCH;
+	}
 	/* convert entered uid to long integer */
 	parmsint = strtol(parms[parm_pos + 1], &endptr, 10);
 	
