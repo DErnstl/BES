80f0286891c41a10985f18cbb73c84d1a156f477
Tom ic14b033 <ic14b033@technikum-wien.at>
2015-03-13 14:58:52 +0100

    try to fix error message for unknown user

--- myfind/myfind.c
+++ myfind/myfind.c
@@ -452,7 +452,7 @@ int check_user(struct stat fd_in, const char * const * parms, int parm_pos)
 	usernam = getpwnam(parms[parm_pos +1]);
 	/* if enterd user is a user name: check if the user exists */
 	if (usernam == NULL) {
-		if (errno == 0) fprintf(stdout, "%s: %s is not the name of a known user", parms[0], parms[parm_pos +1]);	
+		if (errno == 0) error(0, 0, "%s is not the name of a known user", parms[parm_pos +1]);
 		return MISMATCH;
 	}
 	/* get user data with UID */
