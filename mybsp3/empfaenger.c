93d045dafca68d8b68c54d0022a0ebaa3cacbae1
beckattack <romeo.beck@yahoo.com>
2015-06-05 12:21:30 +0200

    Update empfaenger.c

--- mybsp3/empfaenger.c
+++ mybsp3/empfaenger.c
@@ -1,14 +1,20 @@
+##
+## @file empfaenger
+## Betriebssysteme mybsp3 Makefile
+## Beispiel 3
+##
+## @author Adam Kerenyi <ic14b080@technikum-wien.at>
+## @author Romeo Beck <ic14b037@technikum-wien.at>
+## @author Thomas Zeitinger <ic14b033@technikum-wien.at>
+## @date 2015/06/05
+##
+## @version $Revision: 972 $
+##
+##
+
 #ifndef MYBSP3_H
 #include "mybsp3.h"
 #endif
-
-/* Ich bin der Sender */
-
-/* TODO:
- * - P
- * - daten rein schreiben
- * - V
- * - Signalbehandlung */
 
 int main(int argc, char* const argv[]) {
 
